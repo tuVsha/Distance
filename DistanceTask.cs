9e919040a058a06a87bf6172d4ed8a98a4418520
tuVsha <55377453+tuVsha@users.noreply.github.com>
2019-09-23 08:10:29 +0500

    Add files via upload

---
+++ DistanceTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DistanceTask
+{
+	public static class DistanceTask
+	{
+		// Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
+		public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+		{
+            var A = Math.Abs(ay - by);
+            var B = Math.Abs(bx - ax);
+            if (A + B != 0) return (Math.Abs(A * x + B * y + ax * by - bx * ay)) / Math.Sqrt(A * A + B * B);
+            else return x * x + y * y;
+		}
+	}
+}
